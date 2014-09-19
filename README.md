d34017cbf7b8a1fca0c43cfacc82da85eae79aed
Tatiana Khair <t.mirkevich@gmail.com>
2014-09-19 23:37:01 +0400

    A version ready for class submission. Text content and photo are updated. iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 P1
 ==
 
-CSCI E-15 by Tatiana Khair
+CSCI E-15 P1 by Tatiana Khair
+Live URL http://cscie15p1.khair.co/
+Github https://github.com/tkhair/P1
---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CSCI E-15 - Tatiana Khair</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Tatiana Khair - CSCI E-15 Class Assignments</h1>
+<!-- Photo -->  
+    <div class="photo"> <img src='images/IMG_3662.JPG' alt='Tatiana Khair' >
+    </div>
+    
+<!-- Nav tabs -->
+<div class="tabs">
+
+  <p>Hi! Welcome to my CSCI E-15 Portfoilio website. Links and notes about my projects can be found below</p>
+    <ul class="nav nav-tabs" role="tablist">
+      <li class="active"><a href="#p2" role="tab" data-toggle="tab">P2: PHP Basics</a></li>
+      <li><a href="#p3" role="tab" data-toggle="tab">P3: Laravel Basics</a></li>
+      <li><a href="#p4" role="tab" data-toggle="tab">P4: Final</a></li>
+    </ul>
+
+    <!-- Tab panes -->
+    <div class="tab-content"  width=70%>
+      <div class="tab-pane active" id="p2">    
+        Available on Oct 3, 2014<br/>
+        View: <a href="http://cscie15p2.khair.co/">http://cscie15p2.khair.co/</a> <br/>
+        Github: <a href="https://github.com/tkhair/P2">https://github.com/tkhair/P2</a></div>
+      <div class="tab-pane" id="p3">
+        Available on Oct 24, 2014<br/>
+        View: <a href="http://cscie15p3.khair.co/">http://cscie15p3.khair.co/</a> <br/>
+        Github: <a href="https://github.com/tkhair/P3">https://github.com/tkhair/P3</a> 
+      </div>
+      <div class="tab-pane" id="p4">    
+        Available on Dec 12, 2014<br/>
+        View: <a href="http://cscie15p4.khair.co/">http://cscie15p4.khair.co/</a> <br/>
+        Github: <a href="https://github.com/tkhair/P4">https://github.com/tkhair/P4</a> 
+      </div>
+    </div>
+</div>
+
+<!-- Text Content -->
+<div class="content">
+    <h2>In 2+ sentences describe who you are and what you do.</h2>
+    <p>I'm 26 years old HES student pursuing my ALM IT degree. For the last 5 years I live in Abu Dhabi, United Arab Emirates (a bit far away from Cambridge!) and building my career in project management. Currently I work for Cloud solutions provider trying to make cloud infrastructure available for various businesses. </p>
+    
+    <h2>Why are you taking this course?</h2>
+    <p>I'd like to expand my knowledge of dynamic web development mainly for personal needs: I frequently have some sort of web-services ideas and want to be able to prototype or even get them done by myself. I was involved in plenty of web development project at the beggining of my career, but now web-interfaces are usually just part of larger projects, and I'd like to keep my awareness of web dev side always high.</p>
+    
+    <h2>What previous experience do you have with programming?</h2>
+    <p>Apart of theoretical background about algorithmisation, DB systems and programming tehcniques I've worked with some LAMP-based open source CMSs (Drupal, Joomla, Wordpress), mainly collecting ready-made components in one custom engine. <br/>
+    I was also kinda PRO in VBA during undergrad days :)</p>
+    
+    <h2>Are you working on a Mac, PC, or some other platform?</h2>
+    <p>I work on a PC.</p>
+    
+    <h2>Anything else we should know?</h2>
+    <p>I'll be using my GoDaddy web-hosting account to host term projects.</p>  
+</div>
+
+<div class="footer">
+    <p>(c) Tatiana Khair 2014<br/>
+    This page have been developed in-line with Assignment 1 requirements of CSCI E-15 class at Harvard Extension School</p
+</div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
